3321b801e96334f4adf15f276b0c0b04884b0886
Max-Baka <maximelanglois@ip-172-31-18-134.eu-west-3.compute.internal>
2022-11-24 11:29:09 +0000

    second

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 window.addEventListener("DOMContentLoaded", function(event){
 
 });
+let sections=document.querySelectorAll("body > main > section")
+
+function getNextSection(current)
+{
+    if (current.id ==="red")
+    {
+        return document.getElementById("orange");
+    }
+}
