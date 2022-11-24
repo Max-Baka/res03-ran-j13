ae38f621a7ee4ba0be0acae35e05c7b7739f21e0
Max-Baka <maximelanglois@ip-172-31-18-134.eu-west-3.compute.internal>
2022-11-24 15:26:33 +0000

    dernier

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 window.addEventListener("DOMContentLoaded", function(event){
 
 
-let sections=document.querySelectorAll("body > main > section");  
-    for(let i=0; i<sections.length ;i++){
-    sections[i].addEventListener("wheel",function(event);
-    }
+    let sections=document.querySelectorAll("body > main > section");  
+    for(let i=0; i<sections.length ;i++){            
+        
+        /*document.getElementById("myDIV").addEventListener("wheel", myFunction);
 
-function getNextSection(current)
-{
-    if (current.id ==="red")
+                function myFunction() {
+                this.style.fontSize = "35px";
+                sections[i].addEventListener("",  */
+    }
+    
+    function getNextSection(current)
     {
-        return document.getElementById("orange");
-    }
-    if (current.id ==="orange"){
-        return document.getElementById("yellow");
-    }
-    if (current.id ==="yellow"){
-        return document.getElementById("green");
-    }
-    if (current.id ==="green"){
-        return document.getElementById("blue");
-    }
-    if (current.id ==="blue"){
-        return document.getElementById("purple");
-    }
-}    
+        if (current.id ==="red")
+        {
+            return document.getElementById("orange");
+        }
+        if (current.id ==="orange"){
+            return document.getElementById("yellow");
+        }
+        if (current.id ==="yellow"){
+            return document.getElementById("green");
+        }
+        if (current.id ==="green"){
+            return document.getElementById("blue");
+        }
+        if (current.id ==="blue"){
+            return document.getElementById("purple");
+        }
+    }    
 });
